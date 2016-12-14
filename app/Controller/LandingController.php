1cb84604f8e89b032f74d1f7c5ccb4e96daf3408
nikoumhook <nikoumhook+github@gmail.com>
2016-12-14 16:04:19 +0100

    ok cassiette en cour debut carte

--- app/Controller/LandingController.php
+++ app/Controller/LandingController.php
@@ -7,9 +7,6 @@ use \W\Controller\Controller;
 use \W\Model\UsersModel;
 
 
-use W\Security\AuthentificationModel;
-use Respect\Validation\Validator as v;
-
 class LandingController extends Controller
 
 {
@@ -24,6 +21,8 @@ class LandingController extends Controller
         if (isset($_SESSION['player']) && !empty($_SESSION['player'])) {
             $this->redirectToRoute('game_assiette');
         }
+
+        
 
 		$this->show('landingPage.php');
 

1f45195d7b04b5ae6f1f58fb1196eef35cc48388
rousrenaud <rous.renaud@gmail.com>
2016-12-16 17:28:28 +0100

    Commit Quizz

--- app/Controller/QuizzController.php
+++ app/Controller/QuizzController.php
@@ -10,18 +10,29 @@ class QuizzController extends Controller
 {
 
     public function quizz(){
+        $e = 1;
         $quizzModel = new QuizzModel();
         $pedagoModel = new PedagoModel();
         $aliments = array_slice(explode(',', $_SESSION['save']['id_quizz']), -3, 3);
-        $question1 = $quizzModel->getQuizzByIdAliment($aliments[0]);
-        $question2 = $quizzModel->getQuizzByIdAliment($aliments[1]);
-        $question3 = $quizzModel->getQuizzByIdAliment($aliments[2]);
-        $titre = $pedagoModel->getQuizzAliment($question1[0]['id_aliment']);
+        for($i = 0; $i < count($aliments); null){
+            ${'question'.$e} = $quizzModel->getQuizzByIdAliment($aliments[$i]);
+            ${'question'.$e} = $pedagoModel->getQuizzAliment(${'question'.$e}[$i]['id_aliment']);
+            $e++;
+            $i++;
+        }
         $this->show('front/quizz',[
-            'titre'     => $titre,
             'question1' => $question1,
             'question2' => $question2,
-            'question3' => $question3,
+            'question3' => $question3
+            // 'question4' => $question4,
+            // 'question5' => $question5,
+            // 'question6' => $question6,
+            // 'question7' => $question7,
+            // 'question8' => $question8,
+            // 'question9' => $question9,
+            // 'question10' => $question10,
+            // 'question11' => $question11,
+            // 'question12' => $question12,
         ]);
     }
 }

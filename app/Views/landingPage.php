097777e9163bba6db4f18af74aaf9d8f0342f0ce
maofiozo <maofiozo+framagit@gmail.com*>
2016-12-20 07:55:26 +0100

    modif landing spacebeetween

--- app/Views/landingPage.php
+++ app/Views/landingPage.php
@@ -44,41 +44,36 @@
 			<div class="grid-2 wrapperPopin flex-container-v has-gutter">
 
 				<!-- Div formulaire connexion Popin -->
-				<div id="connexion" class="bloc1 flex-container-v">
+				<div id="connexion" class="bloc1 bloc">
 
-					<div class="flex-container-v">
-						<form id="FormConnect" class="" action="#">
-						<h1 class="txtcenter pam titreConnect"> CONNECTE TOI !</h1>
+						<form id="FormConnect" class="bloc w100 h100" action="#">
+						        <h1 class="txtcenter pam titreConnect w100"> CONNECTE TOI !</h1>
 
-							<div class="flex-container-v flex-container">
-								<div class="grid-2 connect flex-container-v">
+								<div class="grid-2 connect w100">
 									<div class="pas">
-										<input id="usernameC" class="inputConnect inputLanding pas" type="text" name="username" placeholder="Ton pseudo" required>
+										<input id="usernameC" class="w100 inputLanding pas" type="text" name="username" placeholder="Ton pseudo" required>
 									</div>
 									<div class="pas txtright">
-										<input id="passwordC" class="inputConnect inputLanding pas" type="password" name="passwordconnect" placeholder="Ton mot de passe" required>
+										<input id="passwordC" class="w100 inputLanding pas" type="password" name="passwordconnect" placeholder="Ton mot de passe" required>
 									</div>
 								</div>
-								<div id="buttonConnexion" class="grid pas">
+                                <!-- div Affichage resultat traitement AJAX CONNEXION-->
+                                <div id="resultConnect" class="mas txtcenter flex-container-v"></div>
+								<div id="buttonConnexion" class="grid pas w100">
 									<button id="validConnexion" class="grid pas enregistrer"type="submit">SE CONNECTER</button>
 								</div>
-							</div>
 
-							<!-- div Affichage resultat traitement AJAX CONNEXION-->
-							<div id="resultConnect" class="mal txtcenter flex-container-v"></div>
 
 						</form>
-					</div>
 
 				</div> <!-- fermeture bloc1 -->
 
 				<!-- Div formulaire inscription Popin -->
-				<div id="inscription" class="bloc2 flex-container-v">
+				<div id="inscription" class="bloc2 bloc">
 
-					<div class="flex-container-v">
-						<form class="" action="#">
+						<form class="bloc" action="#">
 						<h1 class="txtcenter pam titreConnect"> INSCRIS TOI !</h1>
-							
+
 							<div class="grid-3 flex-container-v">
 								<div class="pas">
 									<input id="firstname" class="inputLanding pas" type="text" name="firstname" placeholder="Ton prénom">
@@ -93,10 +88,10 @@
 
 							<div class="grid-2 flex-container-v">
 								<div class="pas">
-									<input id="password" class="inputPassword inputLanding pas" type="password" name="password" placeholder="Ton mot de passe">
+									<input id="password" class="w100 inputLanding pas" type="password" name="password" placeholder="Ton mot de passe">
 								</div>
 								<div class="pas txtright">
-									<input id="passwordVerify" class="inputPassword inputLanding pas" type="password" name="passwordVerify" placeholder="Ton mot de passe">
+									<input id="passwordVerify" class="w100 inputLanding pas" type="password" name="passwordVerify" placeholder="Ton mot de passe">
 								</div>
 							</div> <!-- fermeture grid2 -->
 
@@ -106,19 +101,17 @@
 								</div>
 
 								<div class="pas txtright">
-									<input id="mail" class="inputLanding pas" type="mail" name="mail" placeholder="Ton mail">
+									<input id="mail" class="w100 inputLanding pas" type="mail" name="mail" placeholder="Ton mail">
 								</div>
 							</div> <!-- fermeture grid2 -->
+
+                            <!-- div Affichage resultat traitement AJAX INSCRIPTION-->
+                            <div id="resultInscription" class="mas txtcenter flex-container-v"></div>
 
 							<div id="buttonSubscribe" class="grid pas">
 								<button id="validInscription" class="grid pas enregistrer" type="submit">S'INSCRIRE</button>
 							</div>
-
-							<!-- div Affichage resultat traitement AJAX INSCRIPTION-->
-							<div id="resultInscription" class="mam txtcenter flex-container-v"></div>
-
 							</form>
-					</div>
 
 				</div> <!-- fermeture de la div inscription -->
 

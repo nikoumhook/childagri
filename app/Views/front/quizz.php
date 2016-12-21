559c9e5ec0848d2f45777d104328d1969b23d0f2
Carinette33 <gtcarine@yahoo.fr>
2016-12-21 13:24:42 +0100

    commit de la muerte
    page quizz et resultat quizz style ok

--- app/Views/front/quizz.php
+++ app/Views/front/quizz.php
@@ -8,25 +8,74 @@
 <?php $this->start('main_content'); ?>
 <script src="https://code.jquery.com/jquery-3.1.1.min.js"></script>
 
-<legend><h1>Quiz</h1></legend><hr>
-<div class="containerQuizz">
-    <div id="quizzForm">
-		<form class="" method="post" name="quizzform">
-			<?php if(!empty($question)): ?>
-			<div class="quizAliment">
-				<h3><?php echo $question[0]['ingredient'] ?></h3>
-				<?php foreach($question as $value): ?>
-					<label for="<?=$value['id']?>"><?=$value['content']?></label>
-					<input type="radio" name="<?=$value['id']?>" value="oui" checked> Oui
-					<input type="radio" name="<?=$value['id']?>" value="non"> Non<br><br>
-				<?php endforeach;?>
-			</div>
-			<?php endif; ?>
-			<br>
-			<button type="submit" id="button">Répondre</button>
-		</form>
+
+    <div id="navTopBar">
+        <div class="pas flex-container-v">
+            <div class="center-wrap">
+              <div class="title-container">
+                <div class="ribbon-left"></div>
+                <div class="backflag-left"></div>
+                <div class="title"><?= strtoupper($_SESSION['player']['username']);?> JOUE AU QUIZZ AVEC NOUS !
+                 <span class="sous-title">
+                     Réponds à chacunes des questions l'une après l' autre
+                 </span>
+                </div>
+                <div class="backflag-right"></div>
+                <div class="ribbon-right"></div>
+              </div>
+            </div>
+            <!-- le contenu ici est remplacé par jQuery quand un repas est selectionné ! -->
+            <!-- <div class="reglesAssiettes1 pas txtcenter">
+                <= strtoupper($_SESSION['player']['username']);?> A TOI DE JOUER POUR DECOUVRIR LES ALIMENTS QUE TU MANGES AU QUOTIDIEN
+            </div>
+            <div class="reglesAssiettes2 pas txtcenter">
+                Cliques sur un repas et des aliments vont apparaitre <br>
+                Choisis 3 aliments que tu souhaites manger et glisse-les dans ton assiette
+            </div> -->
+        </div>
     </div>
-</div>
+
+	<div class="pal containerQuizz">
+	    <div id="quizzForm">
+
+			<form class="mal" method="post" name="quizzform">
+
+				<?php if(!empty($question)): ?>
+
+				<div class="quizzAliment">
+
+					<h1 class="txtcenter"><?= ucfirst($question[0]['ingredient']) ?></h1>
+
+					<?php foreach($question as $value): ?>
+
+						<label class="txtcenter" for="<?=$value['id']?>"><?=$value['content']?></label>
+
+						<div class="grid-6 flex-container-v">
+
+							<div class="push">
+								OUI <input type="radio" name="<?=$value['id']?>" value="oui" checked>
+							</div>
+							<div class="pull">
+								<input type="radio" name="<?=$value['id']?>" value="non"> NON
+							</div>
+						</div>
+
+					<?php endforeach;?>
+
+						<div class="grid txtcenter mam">
+							<button type="submit" id="button" class="bouttonEnregistrerQuizz">REPONDRE</button>
+						</div>
+				</div>
+
+
+				<?php endif; ?>
+
+				
+
+
+			</form>
+	    </div>
+	</div>
 
 
 
